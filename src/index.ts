15b43c53885b09dacb9039338866e84628a1671f
JibranMachiavelli <jibranind31@gmail.com>
2025-06-04 01:34:22 -0300

    add gitignore

--- src/index.ts
+++ src/index.ts
@@ -8,5 +8,5 @@ app.get('/healthcheck', (req: Request, res: Response) => {
 });
 
 app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+    console.log(`Server is running on port ${PORT}.`);
 });
