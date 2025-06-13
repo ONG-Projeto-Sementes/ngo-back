69495e6f4e39e6c84fcfdd07cddabcebbc6e827e
LeonardoMachiavelli <leonardo_jibran@zensa.com.br>
2025-06-13 14:22:42 -0300

    fix: build errors

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,45 @@
 import dotenv from "dotenv";
+
 dotenv.config();
 
 import cors from "cors";
 import express from "express";
 import bodyParser from "body-parser";
 import compression from "compression";
-import router from "./router/index.js";
 import cookieParser from "cookie-parser";
+
+import router from "./router/index.js";
 import {connection} from "./core/connection.js";
-import { ErrorHandler } from "./middlewares/ErrorHandler.js";
+import {ErrorHandler} from "./middlewares/ErrorHandler.js";
+import * as process from "node:process";
 
 const app = express();
 const PORT = process.env.PORT || 8000;
 
-
-void connection(() => {
-    app.use(
-      cors({
+app.use(
+    cors({
         origin: "http://localhost:5173",
         credentials: true,
-      }),
-    );
+    }),
+);
 
-    app.use(compression());
-    app.use(cookieParser());
-    app.use(bodyParser.json());
+app.use(compression());
+app.use(cookieParser());
+app.use(bodyParser.json());
 
+app.get("/health", (req, res) => {
+    res.status(200).json({
+        status: "OK",
+        uptime: process.uptime(),
+    });
+});
+
+app.use("/", router());
+
+app.use(ErrorHandler);
+
+void connection(() => {
     app.listen(PORT, () => {
-      console.log(`Server is running on http://localhost:${PORT}`);
+        console.log(`🚀 Server is running on port ${PORT}`);
     });
-
-    app.get("/health", (req, res) => {
-      res.status(200).json({ status: "OK", uptime: process.uptime() });
-    });
-
-    app.use("/", router());
-
-    app.use(ErrorHandler);
-})
-
+});
