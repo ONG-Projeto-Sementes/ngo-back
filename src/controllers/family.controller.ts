35746c85332352c8e523a69f33b444de28cc3eb7
gabrieltakaki <gabitj@icloud.com>
2025-06-14 11:41:51 -0300

    refactor: update `FamilyService` instantiation to improve import consistency
    - Replaced `FamilyService` class instantiation with a direct import of the default `familyService`.
    - Simplified `family.controller.ts` to enhance module consistency.

--- src/controllers/family.controller.ts
+++ src/controllers/family.controller.ts
@@ -1,12 +1,11 @@
 import Joi from "joi";
 import express from "express";
 
-import { FamilyService } from "../services/family.service.js";
+import familyService from "../services/family.service.js";
 import { AsyncHandler } from "../middlewares/AsyncHandler.js";
 import { BodyHandler } from "../middlewares/BodyHandler.js";
 import { BeneficiaryService } from "../services/beneficiary.service.js";
 
-const familyService = new FamilyService();
 const beneficiaryService = new BeneficiaryService();
 
 export const getAllFamilies = AsyncHandler(
