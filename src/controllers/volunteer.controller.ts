cd724de3de4264be69166b4f058c0a854145630c
gabrieltakaki <gabitj@icloud.com>
2025-06-19 15:06:36 -0300

    feat: add image upload support to volunteers and integrate S3 storage
    - Introduced `FileHandler` middleware for handling image uploads in volunteer endpoints.
    - Added `insertOneWithImage` and `updateOneWithImage` methods to `VolunteerService` for managing images.
    - Integrated AWS S3 via `fileApi` for image storage and retrieval.
    - Enhanced `createVolunteer` and `updateVolunteer` controllers for image processing.

--- src/controllers/volunteer.controller.ts
+++ src/controllers/volunteer.controller.ts
@@ -4,6 +4,7 @@ import express from "express";
 import { BodyHandler } from "../middlewares/BodyHandler.js";
 import { AsyncHandler } from "../middlewares/AsyncHandler.js";
 import { VolunteerService } from "../services/volunteer.service.js";
+import { FileHandler } from "../middlewares/FileHandler.js";
 
 const volunteerService = new VolunteerService();
 
@@ -14,6 +15,7 @@ export const getVolunteers = [
 ];
 
 export const createVolunteer = [
+  FileHandler([{ name: "image", maxCount: 1 }]),
   BodyHandler(
     Joi.object({
       name: Joi.string().required(),
@@ -23,6 +25,38 @@ export const createVolunteer = [
     }),
   ),
   AsyncHandler(async (req: express.Request, res: express.Response) => {
-    res.status(201).json(await volunteerService.insert(req.body));
+    res
+      .status(201)
+      .json(
+        await volunteerService.insertOneWithImage(
+          req.body,
+          (req.files as { image: Express.Multer.File[] })?.image?.[0],
+        ),
+      );
+  }),
+];
+
+export const updateVolunteer = [
+  FileHandler([{ name: "image", maxCount: 1 }]),
+  BodyHandler(
+    Joi.object({
+      name: Joi.string().optional(),
+      cpf: Joi.string().optional(),
+      contact: Joi.string().optional(),
+      birthDate: Joi.date().optional(),
+    }),
+  ),
+  AsyncHandler(async (req: express.Request, res: express.Response) => {
+    const { id } = req.params;
+
+    res
+      .status(200)
+      .json(
+        await volunteerService.updateOneWithImage(
+          id,
+          req.body,
+          (req.files as { image: Express.Multer.File[] })?.image?.[0],
+        ),
+      );
   }),
 ];
--- src/services/volunteer.service.ts
+++ src/services/volunteer.service.ts
@@ -1,9 +1,73 @@
 import { BaseService } from "../core/base-service/index.js";
 import { IVolunteer, VolunteerModel } from "../models/volunteer.js";
+import { Mutable } from "../core/base-service/interface.js";
+import { randomUUID } from "node:crypto";
+import { fileApi } from "../apis/file.api.js";
+import { ObjectId } from "mongoose";
+import { NotFoundError } from "../errors/not-found.error.js";
 
 export class VolunteerService extends BaseService<IVolunteer> {
   constructor() {
     super(VolunteerModel);
+  }
+
+  async insertOneWithImage(
+    data: Mutable<IVolunteer>,
+    image?: Express.Multer.File,
+  ): Promise<IVolunteer> {
+    const event = await super.insert(data);
+
+    if (image) {
+      const imagePath = `volunteer/${
+        event._id
+      }/${randomUUID()}.${image.originalname.split(".").pop()}`;
+
+      await fileApi.upload({
+        key: imagePath,
+        data: image.buffer,
+      });
+      data.profilePicture = `https://ong-sementes.s3.sa-east-1.amazonaws.com/${imagePath}`;
+    }
+
+    return (await super.updateOne(event._id, data)) as IVolunteer;
+  }
+
+  async updateOneWithImage(
+    id: string | ObjectId,
+    data: Partial<IVolunteer>,
+    image?: Express.Multer.File,
+  ): Promise<IVolunteer | null> {
+    const event = await super.findById(id);
+
+    if (!event) {
+      throw new NotFoundError("event_not_found", "Event não encontrado.");
+    }
+
+    if (image) {
+      const folder = await fileApi.list({
+        path: `volunteer/${event._id.toString()}`,
+      });
+      if (folder && folder.Contents?.length) {
+        const promises = folder.Contents.map(async (folderFile) =>
+          folderFile.Key
+            ? fileApi.delete({ key: folderFile.Key })
+            : Promise.resolve(),
+        );
+        await Promise.all(promises);
+      }
+
+      const imagePath = `volunteer/${
+        event._id
+      }/${randomUUID()}.${image.originalname.split(".").pop()}`;
+      await fileApi.upload({
+        key: imagePath,
+        data: image.buffer,
+      });
+
+      data.profilePicture = `https://ong-sementes.s3.sa-east-1.amazonaws.com/${imagePath}`;
+    }
+
+    return super.updateOne(id, data);
   }
 }
 
